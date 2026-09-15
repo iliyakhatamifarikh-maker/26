f50e2c6bcde619049406c473f70f121e28e249f3
iliya khatami far <iliyakhatamifarikh@gmail.com>
2026-09-15 09:40:47 +0330

    Add files via upload

---
+++ GPT8888.py
@@ -0,0 +1,9 @@
+ikh=input("mablagh ra vared konid :")
+ikh2=int(ikh)
+ikh3=ikh2 // 3
+ikh7=int(ikh3)
+ikh4=ikh7 * 3 
+ikh6=int(ikh4)
+ikh5=ikh2 - ikh6
+print(f"sahme har nafar {ikh3} ast.")
+print(f"baghi mandeh :{ikh5}")
